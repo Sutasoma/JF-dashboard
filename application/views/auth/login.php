32578223602d17225342e8696b809bbbd3426a52
Kehati <kehati.id@gmail.com>
2018-10-13 08:35:53 +0700

    coreui

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,41 +5,69 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Jasindo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/Ionicons/css/ionicons.min.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/iCheck/square/blue.css')?>">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/node_modules/@coreui/icons/css/coreui-icons.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/node_modules/flag-icon-css/css/flag-icon.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/node_modules/font-awesome/css/font-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/node_modules/simple-line-icons/css/simple-line-icons.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/vendors/pace-progress/css/pace.min.css')?>">
 </head>
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href=""><b>Jasindo</b> Finology</a>
-        </div>
-
-        <div class="login-box-body">
-            <p class="login-box-msg">Sign in to start your session</p>
-
-            <form action="../../index2.html" method="post">
-                <div class="form-group has-feedback">
-                    <input type="email" class="form-control" placeholder="Username">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+<body class="app flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card-group">
+                    <div class="card p-4">
+                        <div class="card-body">
+                            <h1>Login</h1>
+                            <p class="text-muted">Sign In to your account</p>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="icon-user"></i>
+                                    </span>
+                                </div>
+                                <input class="form-control" type="text" placeholder="Username">
+                            </div>
+                            <div class="input-group mb-4">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="icon-lock"></i>
+                                    </span>
+                                </div>
+                                <input class="form-control" type="password" placeholder="Password">
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <button class="btn btn-primary px-4" type="button">Login</button>
+                                </div>
+                                <div class="col-6 text-right">
+                                    <button class="btn btn-link px-0" type="button">Forgot password?</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
+                        <div class="card-body text-center">
+                            <div>
+                                <h2>Sign up</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <button class="btn btn-primary active mt-3" type="button">Register Now!</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <p class="text-right">
-                    <a href="#">Forgot Password</a>
-                </p>
-                <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-            </form>
+            </div>
         </div>
     </div>
 
-<script src="<?php echo base_url('assets/bower_components/jquery/dist/jquery.min.js')?>"></script>
-<script src="<?php echo base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/node_modules/jquery/dist/jquery.min.js')?>"></script>
+<script src="<?php echo base_url('assets/node_modules/popper.js/dist/umd/popper.min.js')?>"></script>
+<script src="<?php echo base_url('assets/node_modules/bootstrap/dist/js/bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/node_modules/pace-progress/pace.min.js')?>"></script>
+<script src="<?php echo base_url('assets/node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js')?>"></script>
+<script src="<?php echo base_url('assets/node_modules/@coreui/coreui/dist/js/coreui.min.js')?>"></script>
 
 </body>
 </html>
